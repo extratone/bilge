676315a397a45c46795afecf2e4883b3c5739c89
David Blue <davidblue@extratone.com>
2021-11-16 19:10:20 -0600

    #219

--- .sophie/plugins/obsidian-style-settings/main.js
+++ .sophie/plugins/obsidian-style-settings/main.js
@@ -3739,7 +3739,14 @@ const cz = {};
 
 const da = {};
 
-const de = {};
+const de = {
+    "Default:": "Standard:",
+    "Error:": "Fehler:",
+    "missing default light value, or value is not in a valid color format": "Fehlender heller standard Wert oder Wert ist in keinem validen Farb-Format",
+    "missing default dark value, or value is not in a valid color format": "Fehlender dunkler standard Wert oder Wert ist in keinem validen Farb-Format",
+    "missing default value, or value is not in a valid color format": "Fehlender standard Wert oder Wert ist in keinem validen Farb-Format",
+    "missing default value": "Fehlender standard Wert",
+};
 
 const en = {
     "Default:": "Default:",
